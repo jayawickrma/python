f6569171cde460d46da787a3239291a664a00212
jayawickrama <nishantharaka2004@gmail.com>
2024-11-12 12:14:19 +0530

    day02

---
+++ day2.py
@@ -0,0 +1,52 @@
+my_list1=[10,20,30,40,50]
+my_list2=[]
+my_list2=my_list1.copy()
+my_list1[1]="nishan"
+# my_list1(my_list2)
+print(my_list1)
+print(my_list2)
+
+my_list2=list(my_list1)
+my_list1[1]=40
+print(my_list1,my_list2)
+
+my_list3=my_list1+my_list2
+print(my_list3,len(my_list3))
+
+
+my_tuple1=("dog","cat","bird","tiger","lion")
+print(my_tuple1.type(my_tuple1))
+
+
+my_tuple1=("dog","cat","bird","tiger","lion")
+# my_list1=list(my_tuple1)            convert tuple1 to list type
+my_list1[1]="nishan"
+# my_tuple1=tuple(my_list1)              convert again to mylist1 to tuple
+print(my_tuple1,type(my_tuple1))
+
+
+my_tuple1=(10,20,32,-5)
+my_tuple2=(5,9,-1)
+my=my_tuple1+my_tuple2
+my.sort(reverse=True)
+print(my)
+
+
+# r1=range(2,20,2) start value =2,end value=20 and print 2x 20 wenakl values
+print(r1,type(r1))
+
+for i in r1:
+    print(i,end=" ")
+
+# dict_2={"name":"nishanTharaka","age":21,"marks":50,"marks":60}          by print method will diaplay last value of marks
+print(dict_2,type(dict_2),len(dict_2))
+
+dict_2={"name":"nishanTharaka","age":21,"marks":50,"marks":60}
+# print(dict_2["name"]) print the value in given index name
+
+
+# dict_1.update({"marks":80})
+# print(dict_1)           update values using update method
+
+# dict_1.popitem()
+# print(dict_1)       remove last item from the dict_1
---
+++ first.py
@@ -0,0 +1,108 @@
+a=100
+print("type of variable having value",a,"is",type(a))
+
+# num1=2.5e4
+# print(num1,type(num1))
+
+
+        # casting
+
+# num2=4
+
+# b=float(num2)
+# print(b,type(b))
+
+# num2=12.4
+# b=int(num2)
+# print(b,type(b))
+
+
+            # python lists
+# my_List=[10,20,30,40]
+# print(my_List,type(my_List))               print arraylist
+# print(my_List[2])                            print the index
+# my_List[1]=25   update tha array index
+# print(my_List)  print the updated array           if the duplicated values in the aray when complile it will be show an error
+
+
+# my_List=[10,20,20,"nishan",30,40,True]
+# print(len(my_List))  to get array length
+
+
+# my_List=[10,20,20,"nishan",30,40,True]
+# print(my_List[-2])                                to get the value from back to fowert from array
+
+
+# my_List=[10,20,20,"nishan",30,40,True]
+# print(my_List[1:3])                                 to get value from array given indexes
+
+# my_List=[10,20,20,"nishan",30,40,True]
+# print(my_List[:3])                                  print values to 0 index to 3rd index
+
+# my_List=[10,20,20,"nishan",30,40,True]
+# print(my_List[3:])                                     prints start from 3rd index and print until over the array
+
+# my_List=[10,20,20,"nishan",30,40,True]
+# my_List[2:3]= ["dog",19.0]
+# print(my_List)                                          update many indexes in one time
+
+# my_List=[10,20,20,"nishan",30,40,True]
+# my_List.insert(3,"Nethmi")
+# print(my_List,len(my_list))                                      insert value after given index value and array length
+
+# my_List=[10,20,20,"nishan",30,40,True]
+# my_List.append("car")
+# print(my_List,len(my_List))                             add e nev value to the last index in array
+
+# my_List=[10,20,20,"nishan",30,40,True]
+# new_List=[10,11,12]
+# my_List.extend(new_List)
+# print(my_List,len(my_List))                             extend another array to mylist array
+
+# my_List=[10,20,20,"nishan",30,40,True]
+
+# my_List.pop(2)
+# print(my_List,len(my_List))                             remove an index from array
+
+# my_List=[10,20,20,"nishan",30,40,True]
+# del my_List[2]
+# print(my_List,len(my_List))                                 delete an index from array
+
+# my_List=[10,20,20,"nishan",30,40,True]
+# del my_List
+# print(len(my_List))                                         array destroyed because will show compile error
+
+# my_List=[10,20,20,"nishan",30,40,True]
+# my_List.clear()
+# print(len(my_List))                                         clear the array
+
+
+# my_List=["abc","bcd","cde","def","aaegh"]
+# my_List.sort()
+# print(my_List)                                                  Sort array
+
+# my_List=["abc","asdcd","cde","def","agh"]
+# my_List.sort(reverse=True)
+# print(my_List)                                                  reverce array in english alphabhat
+
+
+# my_List=["abc","asdcd","cde","def","agh"]
+# my_List.sort(key=str.lower())
+# print(my_List)                                                          compile erroe because case sensitive
+
+
+
+# import statistics 
+
+
+# my_List1=[-10,-18,12,5,7,12,-3]
+# my_List2=[8,4,-3,9,2,11]
+# my_List1.extend(my_List2)
+
+
+# print("The original list : " + str(my_List1)) 
+
+# res = statistics.median(my_List1) 
+
+# print("Median of list is : " + str(res)) 
+#                                                                 find median of these arrays in extended array
